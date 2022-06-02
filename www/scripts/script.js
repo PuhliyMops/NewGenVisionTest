999b6d57d232423344ec4ad3d1a7a5b441b0c120
PuhliyMops <kb36@list.ru>
2022-06-02 15:15:11 +0300

    almost done

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,12 +1,3 @@
-document.querySelector('.j-courses-item').addEventListener('click');
-
-function preventEvent(event) {
-  event.preventDefault()
-}
-
-
-
-// Список курсов
 let courses = [
   { name: "Courses in England", prices: [0, 100] },
   { name: "Courses in Germany", prices: [500, null] },
@@ -22,3 +13,42 @@ let courses = [
 let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
 let requiredRange3 = [200, null];
+
+function findCourses(range) {
+const left = range[0]
+const right = range[1]
+
+return courses.filter(course=>{
+const courseLeft = course.prices[0]
+const courseRight = course.prices[1]
+
+if(!courseLeft && !courseRight) {
+return true
+}
+
+if(left && right) {
+      console.log('both')
+
+return left <= courseLeft && courseRight <= right
+}
+
+
+if(left) {
+      console.log('left')
+
+return left <= courseLeft && left <= courseRight
+}
+
+if(right) {
+
+console.log('right')
+return courseLeft <= right && courseRight <= right
+
+}
+
+}).map((course)=>course.name)
+}
+
+console.log(findCourses(requiredRange1))
+console.log(findCourses(requiredRange2))
+console.log(findCourses(requiredRange3))
